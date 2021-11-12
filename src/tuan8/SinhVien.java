e5357b6c251c860765da4f3166cb026122ba2d34
lenguyentantai <72006772+nohope2305@users.noreply.github.com>
2021-11-12 08:13:08 +0700

    first commit

---
+++ src/tuan8/SinhVien.java
@@ -0,0 +1,19 @@
+package tuan8;
+
+public class SinhVien {
+	private String tenSV;
+
+	public String getTenSV() {
+		return tenSV;
+	}
+
+	public void setTenSV(String tenSV) {
+		this.tenSV = tenSV;
+	}
+
+	public SinhVien(String tenSV) {
+		super();
+		this.tenSV = tenSV;
+	}
+	
+}
